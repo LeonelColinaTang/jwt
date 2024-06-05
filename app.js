b78a65d44e7c7b3be75e5ee80d3e9d327a15ae37
leonelcolina <tang.leonel@gmail.com>
2024-06-05 17:41:47 -0400

    JWT set

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 require('dotenv').config();
 
 // Import package
-
-// Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 
@@ -11,6 +10,11 @@ require('dotenv').config();
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
 // Your code here
+token = jwt.sign(
+    {email: "leonel@gmail.com"}, //payload object
+    process.env.SECRET_KEY, //secret token from .env file
+    {expiresIn: '1h'} // options (example: Token expires in one hour)
+)
 
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
@@ -20,7 +24,7 @@ console.log('JWT:', token);
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
 // Your code here
-
+payload = jwt.decode(token);
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
 
@@ -29,6 +33,7 @@ console.log('Payload:', payload);
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
 // Your code here
+verifiedPayload = jwt.verify(token, process.env.SECRET_KEY)
 
 // See the verified payload in the console - DO NOT MODIFY
 console.log('Verified Payload:', verifiedPayload);
